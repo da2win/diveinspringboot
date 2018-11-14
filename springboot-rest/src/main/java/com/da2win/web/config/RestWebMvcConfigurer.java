00c6c5e920001b8f4f968c45b118dfb0cf65e124
Darwin <da2win@foxmail.com>
2018-11-14 17:28:39 +0800

    rest: custom resolver

--- springboot-rest/src/main/java/com/da2win/web/config/RestWebMvcConfigurer.java
+++ springboot-rest/src/main/java/com/da2win/web/config/RestWebMvcConfigurer.java
@@ -1,10 +1,16 @@
 package com.da2win.web.config;
 
 import com.da2win.web.http.converter.properties.PropertiesHttpMessageConverter;
+import com.da2win.web.support.PropertiesHandlerMethodArgumentResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,11 +20,42 @@ import java.util.List;
  */
 @Configuration
 public class RestWebMvcConfigurer implements WebMvcConfigurer {
+    @Autowired
+    private RequestMappingHandlerAdapter requestMappingHandlerAdapter;
+
+    @PostConstruct
+    public void init() {
+
+        // 获取当前 requestMappingHandlerAdapter 所有的 Resolver 对象
+        List<HandlerMethodArgumentResolver> argumentResolvers = requestMappingHandlerAdapter.getArgumentResolvers();
+
+        List<HandlerMethodArgumentResolver> newResolvers = new ArrayList(argumentResolvers.size() + 1);
+
+        // 添加 PropertiesHandlerMethodArgumentResolver 到集合首位
+        newResolvers.add(new PropertiesHandlerMethodArgumentResolver());
+
+        // 添加 已注册的 Resolver 对象集合
+        newResolvers.addAll(argumentResolvers);
+
+        // 重新设置 Resolver 对象
+        requestMappingHandlerAdapter.setArgumentResolvers(newResolvers);
+    }
 
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
         // 不建议添加到 converters 的末尾
         //converters.add(new PropertiesHttpMessageConverter());
         converters.set(0, new PropertiesHttpMessageConverter()); // 添加至首位
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        // 添加 PropertiesHandlerMethodArgumentResolver 到集合首位
+        // 添加自定义的 HandlerMethodArgumentResolver 优先级低于内建(built-in)的实现
+        //if (resolvers.isEmpty()) {
+        //    resolvers.add(new PropertiesHandlerMethodArgumentResolver());
+        //} else {
+        //    resolvers.set(0, new PropertiesHandlerMethodArgumentResolver());
+        //}
     }
 }
---
+++ springboot-rest/src/main/java/com/da2win/web/support/PropertiesHandlerMethodArgumentResolver.java
@@ -0,0 +1,55 @@
+package com.da2win.web.support;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.Properties;
+
+/**
+ * {@link Properties} {@link HandlerMethodArgumentResolver}
+ *
+ * @Author Darwin
+ * @Date 2018/11/14 16:44
+ */
+public class PropertiesHandlerMethodArgumentResolver implements HandlerMethodArgumentResolver {
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return Properties.class.equals(parameter.getParameterType());
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+
+        ServletWebRequest servletWebRequest = (ServletWebRequest) webRequest;
+
+        // Servlet Request API 实现
+        HttpServletRequest request = (HttpServletRequest) servletWebRequest.getNativeRequest();
+
+        String contentType = request.getHeader("Content-Type");
+        // 获取
+        MediaType mediaType = MediaType.parseMediaType(contentType);
+
+        Charset charset = mediaType.getCharset() == null ? Charset.forName("UTF-8") : mediaType.getCharset();
+
+        InputStream inputStream = request.getInputStream();
+
+        InputStreamReader reader = new InputStreamReader(inputStream, charset);
+
+        Properties properties = new Properties();
+
+        // 加载字符流
+        properties.load(reader);
+
+        return properties;
+    }
+}
