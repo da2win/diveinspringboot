35b919332fd8a581a2e16151008664d5afac286a
Darwin <da2win@foxmail.com>
2018-11-15 13:37:54 +0800

    servlet: async servlet

--- spring-servlet/src/main/java/com/da2win/spring/web/servlet/controller/HelloWorldAsyncController.java
+++ spring-servlet/src/main/java/com/da2win/spring/web/servlet/controller/HelloWorldAsyncController.java
@@ -1,8 +1,14 @@
 package com.da2win.spring.web.servlet.controller;
 
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.async.DeferredResult;
+
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 /**
  * Hello World 异步 {@link RestController}
@@ -12,21 +18,47 @@ import org.springframework.web.context.request.async.DeferredResult;
  */
 
 @RestController
+@EnableScheduling
 public class HelloWorldAsyncController {
+
+    private final BlockingQueue<DeferredResult<String>> queue = new ArrayBlockingQueue<>(5);
+
+    // 超时随机数
+    private final Random random = new Random();
+
+    @Scheduled(fixedRate = 5000)
+    public void process() throws InterruptedException { // 定时操作
+        DeferredResult<String> result = null;
+        do {
+            result = queue.take();
+            // 随机超时时间
+            long timeout = random.nextInt(100);
+            // 模拟等待时间, RPC 或者 DB 查询
+            Thread.sleep(timeout);
+            // 计算结果
+            result.setResult("Hello,World");
+            println("执行计算结果, 消耗: " + timeout + "ms. ");
+
+        } while (result != null);
+    }
 
     @GetMapping("/hello-world")
     public DeferredResult<String> helloWorld() {
-        DeferredResult<String> deferredResult = new DeferredResult<>(50L);
+        DeferredResult<String> deferredResult = new DeferredResult<>();
 
         //deferredResult.setResult("Hello, World");
+        // 入队操作
+        queue.offer(deferredResult);
+
+
         println("hello, world");
 
         deferredResult.onCompletion(() -> {
-            println("执行结束");
+            println("执行结束xxxx");
         });
 
         deferredResult.onTimeout(() -> {
-            println("执行超时");
+            println("执行超时xxxx");
         });
         return deferredResult;
     }
