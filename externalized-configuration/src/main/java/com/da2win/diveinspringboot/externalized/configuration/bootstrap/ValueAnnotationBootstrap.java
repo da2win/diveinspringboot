aa0de430c08c9386ed6ea28d71d4e41485c7f499
Darwin <da2win@foxmail.com>
2018-11-26 16:30:06 +0800

    externalized-configuration: Environment

---
+++ externalized-configuration/src/main/java/com/da2win/diveinspringboot/externalized/configuration/bootstrap/ValueAnnotationBootstrap.java
@@ -0,0 +1,80 @@
+package com.da2win.diveinspringboot.externalized.configuration.bootstrap;
+
+import com.da2win.diveinspringboot.externalized.configuration.domain.User;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+/**
+ * @Author Darwin
+ * @Date 2018/11/26 16:17
+ */
+@EnableAutoConfiguration
+public class ValueAnnotationBootstrap implements BeanFactoryAware {
+
+
+    @Autowired
+    private Environment environment;
+
+    //public ValueAnnotationBootstrap(Environment environment) {
+    //    this.environment = environment;
+    //}
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        this.environment = beanFactory.getBean(Environment.class);
+    }
+
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context =
+                new SpringApplicationBuilder(ValueAnnotationBootstrap.class)
+                        .web(WebApplicationType.NONE) // 非 Web 应用
+                        .run(args);
+
+        User user = context.getBean("user", User.class);
+        User user2 = context.getBean("user2", User.class);
+
+        System.err.println("用户对象 : " + user);
+        System.err.println("用户对象2 : " + user2);
+
+        // 关闭上下文
+        context.close();
+    }
+
+    @Bean
+    public User user(@Value("${user.id}") Long id, @Value("${user.name}") String name,
+                     @Value("${user.age:${my.user.age:32}}") Integer age) {
+        User user = new User();
+
+        user.setId(id);
+        user.setName(name);
+        user.setAge(age);
+        return user;
+    }
+
+    @Bean
+//    @Autowired
+    public User user2(
+            //Environment environment
+    ) {
+        Long id = environment.getRequiredProperty("user.id", Long.class);
+        String name = environment.getRequiredProperty("user.name");
+        Integer age = environment.getProperty("user.age", Integer.class,
+                environment.getProperty("my.user.age", Integer.class, 32));
+        User user = new User();
+        user.setId(id);
+        user.setName(name);
+        user.setAge(age);
+        return user;
+    }
+
+
+}
